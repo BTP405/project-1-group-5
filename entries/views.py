92160388db26f5d7b013632db57be5525b70ef89
ZprKris <zaporozhetskristina@gmail.com>
2024-02-10 01:58:21 -0500

    initial commit

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
+
+from .models import Entry
+
+
+# inherit from ListView class 
+class EntryListView(ListView):
+    # specify which model should be used for retrieving the data to display in the view
+    model = Entry
+    # set entries in ascending order by date 
+    queryset = Entry.objects.all().order_by("-date_created")
+
+# inherit from DetailView class 
+class EntryDetailView(DetailView):
+    model = Entry
