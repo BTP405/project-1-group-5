e8a146891ef2ed3b2cc047019d0095b0d5ccfd91
ZprKris <zaporozhetskristina@gmail.com>
2024-02-16 15:29:59 -0500

    initial commit

--- entries/views.py
+++ entries/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 from django.views.generic import (
     ListView,
     DetailView,
+
+    CreateView,
+    UpdateView,
+    DeleteView,
 )
 
 from .models import Entry
@@ -19,3 +23,29 @@ class EntryListView(ListView):
 # inherit from DetailView class 
 class EntryDetailView(DetailView):
     model = Entry
+
+# ---------------------------------------------
+# CRUD operations 
+    
+# class EntryCreateView(CreateView):
+#     model = Entry
+#     # define which model fields should be displayed in the form
+#     fields = ["title", "content"]
+#     # reverse_lazy() is basically a method to retrieve the url by its name 
+#     success_url = reverse_lazy("entry-list")
+
+# class EntryUpdateView(UpdateView):
+#     model = Entry
+#     fields = ["title", "content"]
+
+#     # get_success_url - just a general practice when you need to dynamically access to additional context 
+#     # get_success_url() simply returns the value of success_url
+#     def get_success_url(self):
+#         return reverse_lazy(
+#             "entry-detail",
+#             {"pk": self.entry.id}
+#         )
+
+# class EntryDeleteView(DeleteView):
+#     model = Entry
+#     success_url = reverse_lazy("entry-list")    
